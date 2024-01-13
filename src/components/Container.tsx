fe120e7cac38d1ce08b6189254862b4551499c64
Eli-Caraballo <eli.caraballojr@gmail.com>
2024-01-13 14:38:30 -0500

    simplify container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,35 +1,25 @@
-import { forwardRef } from 'react'
 import clsx from 'clsx'
 
-export const ContainerOuter = forwardRef<
-	React.ElementRef<'div'>,
-	React.ComponentPropsWithoutRef<'div'>
->(function OuterContainer({ className, children, ...props }, ref) {
+export function OuterContainer({ className, children, ...props }: React.ComponentPropsWithoutRef<'div'>) {
 	return (
-		<div ref={ref} className={clsx('sm:px-8', className)} {...props}>
+		<div className={clsx('sm:px-8', className)} {...props}>
 			<div className="mx-auto w-full max-w-7xl lg:px-8">{children}</div>
 		</div>
 	)
-})
+}
 
-export const ContainerInner = forwardRef<
-	React.ElementRef<'div'>,
-	React.ComponentPropsWithoutRef<'div'>
->(function InnerContainer({ className, children, ...props }, ref) {
+export function InnerContainer({ className, children, ...props }: React.ComponentPropsWithoutRef<'div'>) {
 	return (
-		<div ref={ref} className={clsx('relative px-4 sm:px-8 lg:px-12', className)} {...props}>
+		<div className={clsx('relative px-4 sm:px-8 lg:px-12', className)} {...props}>
 			<div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
 		</div>
 	)
-})
+}
 
-export const Container = forwardRef<
-	React.ElementRef<typeof ContainerOuter>,
-	React.ComponentPropsWithoutRef<typeof ContainerOuter>
->(function Container({ children, ...props }, ref) {
+export function Container({ children, ...props }: React.ComponentPropsWithoutRef<'div'>) {
 	return (
-		<ContainerOuter ref={ref} {...props}>
-			<ContainerInner>{children}</ContainerInner>
-		</ContainerOuter>
+		<OuterContainer {...props}>
+			<InnerContainer>{children}</InnerContainer>
+		</OuterContainer>
 	)
-})
+}
