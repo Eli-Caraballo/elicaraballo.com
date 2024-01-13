6eb9e911782d1a4e4ed1f4133c8ef1f5c4cbe8d1
Eli-Caraballo <eli.caraballojr@gmail.com>
2024-01-13 16:28:50 -0500

    simplify footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,36 +1,17 @@
-import Link from 'next/link'
-
-import { ContainerInner, ContainerOuter } from '@/components/Container'
-
-function NavLink({
-	href,
-	children,
-}: {
-	href: string
-	children: React.ReactNode
-}) {
-	return (
-		<Link
-			href={href}
-			className="transition hover:text-teal-500 dark:hover:text-teal-400"
-		>
-			{children}
-		</Link>
-	)
-}
+import { OuterContainer, InnerContainer } from '@/components/Container'
 
 export function Footer() {
 	return (
-		<footer className="mt-32">
-			<ContainerOuter>
+		<footer className="mt-20 lg:mt-28">
+			<OuterContainer>
 				<div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
-					<ContainerInner>
+					<InnerContainer>
 						<div className="flex justify-end">
 							<p className="text-sm text-zinc-400 dark:text-zinc-500">&copy; {new Date().getFullYear()} Spencer Sharp. All rights reserved.</p>
 						</div>
-					</ContainerInner>
+					</InnerContainer>
 				</div>
-			</ContainerOuter>
+			</OuterContainer>
 		</footer>
 	)
 }
