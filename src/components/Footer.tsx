71ccf2b87e3d51d78798036d7c62d4a9724bf4c6
Eli-Caraballo <eli.caraballojr@gmail.com>
2024-01-14 18:09:05 -0500

    small footer tweaks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,14 @@
 import { OuterContainer, InnerContainer } from '@/components/Container'
+import Link from 'next/link'
 
 export function Footer() {
 	return (
 		<footer className="mt-20 lg:mt-28">
 			<OuterContainer>
-				<div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
+				<div className="border-t border-zinc-200 pb-16 pt-10 dark:border-zinc-700/30">
 					<InnerContainer>
 						<div className="flex justify-end">
-							<p className="text-sm text-zinc-400 dark:text-zinc-500">&copy; {new Date().getFullYear()} Spencer Sharp. All rights reserved.</p>
+							<p className="text-xs text-zinc-400 sm:text-sm dark:text-zinc-500">&copy; {new Date().getFullYear()} Eliezer Caraballo. All rights reserved.</p>
 						</div>
 					</InnerContainer>
 				</div>
